e0530c14b926074d643deb6a44c9a425ee7b26b1
Gian Arias <ariasgian@gmail.com>
2025-08-14 08:03:20 -0300

    Refactor esquema de base de datos: eliminar tablas obsoletas y crear nuevas tablas para producción de petróleo y gas, ajustando nombres de columnas a 'county' y 'year_month'.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,36 +1,36 @@
 PRAGMA foreign_keys = ON;
 
 -- Borra las tablas existentes en orden inverso de dependencia
-DROP TABLE IF EXISTS production_volumes;
-DROP TABLE IF EXISTS wells;
+DROP TABLE IF EXISTS oil_production;
+DROP TABLE IF EXISTS gas_production;
+DROP TABLE IF EXISTS wells_by_county;
 
 -- Tabla de Metadatos para los Pozos (Wells)
-CREATE TABLE wells (
-    id INTEGER PRIMARY KEY,
-    location TEXT NOT NULL,    
+CREATE TABLE wells_by_county (
+    id INTEGER PRIMARY KEY ,
+    county TEXT NOT NULL,    
     operator INTEGER NOT NULL,
     status INTEGER NOT NULL,
     longitude REAL NOT NULL,
-    latitude REAL NOT NULL
-    
+    latitude REAL NOT NULL   
 
 );
 
 -- Tabla de Hechos para los Volúmenes de Producción Mensual
-CREATE TABLE production_volumes (
-    id INTEGER NOT NULL,
+CREATE TABLE oil_production (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     year_month Date,
     production REAL NULL,
-    State TEXT NOT NULL, -- ej. 'Thousand Barrels', 'MMcf'
-    
-    FOREIGN KEY(id) REFERENCES wells(id),
-    
-    -- Restricción para asegurar que solo haya un registro por pozo, fecha y tipo de producto
-    UNIQUE (id, year_month, State)
+    county TEXT NOT NULL -- ej. 'Thousand Barrels', 'MMcf'
+);
+CREATE TABLE gas_production (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year_month Date,
+    production REAL NULL,
+    county TEXT NOT NULL -- ej. 'Thousand Barrels', 'MMcf'
 );
 
 -- Índices para mejorar el rendimiento de las consultas
-CREATE INDEX idx_wells_id ON wells(id);
-CREATE INDEX idx_wells_operator ON wells(operator);
-CREATE INDEX idx_production_id ON production_volumes(id);
-CREATE INDEX idx_production_year_month ON production_volumes(year_month);
+CREATE INDEX idx_oil_production_county_month ON oil_production(county, year_month);
+CREATE INDEX idx_gas_production_county_month ON gas_production(county, year_month);
+CREATE INDEX idx_wells_by_county_county ON wells_by_county(county);
