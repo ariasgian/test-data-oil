6a1802813b1c1eb93f16bc3505d24254005c4c14
Gian Arias <ariasgian@gmail.com>
2025-08-13 23:09:45 -0300

    Agregar clase DataExtractor para la extracción y procesamiento de datos de producción de petróleo y gas

--- etl/extract_data.py
+++ etl/extract_data.py
@@ -7,21 +7,37 @@ from io import BytesIO
 class DataExtractor:
     def get_dataframe(self, url: str, sheet_name: str, skiprows: int) -> pd.DataFrame:
         df = pd.read_excel(url, sheet_name=sheet_name, skiprows=skiprows, engine='openpyxl')
-        # Reemplaza saltos de línea en los nombres de las columnas por espacios
-        df.columns = [col.replace('\n', ' ') for col in df.columns]
+        # Reemplaza saltos de línea en los nombres de las columnas por espacios        
+        df = df.iloc[:, [0, 2]]
+        df.columns = ['month', 'production']
         # Agrega columna con el nombre del estado
         df['State'] = sheet_name
         return df
-    def download_and_extract_csv(self, zip_url: str, extract_to: Path, csv_filename: str) -> pd.DataFrame:
+    def download_and_extract_csv(self, zip_url: str, extract_to: Path, csv_filename: str, columns: list = None) -> pd.DataFrame:
         response = requests.get(zip_url)
         response.raise_for_status()
         with zipfile.ZipFile(BytesIO(response.content)) as z:
             z.extract(csv_filename, path=extract_to)
             csv_path = extract_to / csv_filename
             df = pd.read_csv(csv_path,low_memory=False)
+            # Filtrar columnas si se especifica (primero)
+            if columns is not None:
+                df = df[columns]
+            # Renombrar columnas
+            rename_dict = {
+                'API_WellNo': 'ID',
+                'Surface_location': 'location',
+                'Operator_number': 'operator',
+                'Well_Status': 'status'
+            }
+            df = df.rename(columns=rename_dict)
+            # Unificar coordenadas
+            if 'Surface_Longitude' in df.columns and 'Surface_latitude' in df.columns:
+                df['coordinates'] = df['Surface_Longitude'].astype(str) + ',' + df['Surface_latitude'].astype(str)
+                df = df.drop(['Surface_Longitude', 'Surface_latitude'], axis=1)
         return df
-extractor = DataExtractor()
 
+extractor = DataExtractor()
 def get_production(url: str, sheet_configs: list) -> pd.DataFrame:
     """
     sheet_configs: lista de tuplas (sheet_name, skiprows)
@@ -48,4 +64,4 @@ def get_gas_production():
     output=Path('data') / 'raw' / 'gas_production.csv'
     df_gas.to_csv(output, index=False)
     return df_gas
-extractor.download_and_extract_csv('https://www.dec.ny.gov/fs/data/wellDOS.zip', Path('data') / 'raw', 'wellspublic.csv')
+
